26499e1ea8dbb2da36a45fa41949d3ddde2d2b03
javier <javier_2790@outlook.com>
2024-06-18 17:03:19 +0200

    add module

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI, Body, Query, Path
-from fastapi.responses import HTMLResponse, RedirectResponse, FileResponse
-from pydantic import BaseModel, Field, field_validator
-from typing import Optional,List
-import datetime
+from fastapi import FastAPI
+from fastapi.requests import Request
+from fastapi.responses import Response, JSONResponse
 from src.routers.movie_router import movie_router
 
 app = FastAPI()
-app.include_router(router=movie_router)
+app.include_router(prefix='/movies', router=movie_router)
 app.title = 'mi first app with fast-api'
+
+@app.middleware('http')
+async def http_error_handle(request:Request, call_next) -> Response | JSONResponse:
+    print('Middleware is running')
+    return await call_next(request)
+
 
 
 
--- src/routers/movie_router.py
+++ src/routers/movie_router.py
@@ -1,36 +1,35 @@
 from typing import List
 from fastapi import Path, Query, APIRouter
-from fastapi.responses import FileResponse
 from src.models.movie_model import Movie, MovieCreate, MovieUpdate
 
 movie_router = APIRouter()
 
 movies : List[Movie] = []
-@movie_router.get("/movies", tags=['Movies'])
+@movie_router.get("/", tags=['Movies'])
 def get_movies() -> List[Movie]:
     return [movie.model_dump() for movie in movies]
 
-@movie_router.get("/movies/{id}", tags=['Movies'])
+@movie_router.get("/{id}", tags=['Movies'])
 def get_movie(id: int = Path(gt=0)) -> Movie | dict:
     for movie in movies:
         if movie.id == id:
             return movie.model_dump()
     return {}
 
-@movie_router.get("/movies/", tags=['Movies'])
+@movie_router.get("/by_category", tags=['Movies'])
 def get_movie_by_category(category: str = Query(min_length=4, max_length=20)) -> Movie | dict:
     for movie in movies:
         if movie.category == category:
             return movie.model_dump()
     return {}
 
-@movie_router.post("/movies", tags=['Movies'])
+@movie_router.post("/", tags=['Movies'])
 def create_movie(movie: MovieCreate) -> List[Movie]:
     movies.append(movie)
     #return [movie.model_dump() for movie in movies]
     return [movie.model_dump() for movie in movies]
 
-@movie_router.put("/movies/{id}", tags=["Movies"])
+@movie_router.put("/{id}", tags=["Movies"])
 def update_movie(
     id: int, movie:MovieUpdate
 ) -> List[Movie]:
@@ -45,7 +44,7 @@ def update_movie(
 
     return [movie.model_dump() for movie in movies]
 
-@movie_router.delete("/movies/{id}", tags=["Movies"])
+@movie_router.delete("/{id}", tags=["Movies"])
 def delete_movie(id: int):
     for movie in movies:
         if movie.id == id:
