58a1bfd6c1fbc8212e060fe084bcba158cc18479
javier <javier_2790@outlook.com>
2024-06-23 22:43:46 +0200

    add new.py

--- new.py
+++ new.py
@@ -2,35 +2,54 @@
 from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel
+from jose import JWTError, jwt
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from passlib.context import CryptContext
 
-class Movie(BaseModel):
-    id:int
-    name:str
-    star:int
+ALGORITHM = "HS256"
+SECRET_KEY = "8488848jjjujh40ojfndfd$1212"
+ACCESS_TOKEN_EXPIRED_MINUTES = 30
+
+class Token(BaseModel):
+    access_token: str
+    type_token: str
+
+class TokenData(BaseModel):
+    username:str
+
+class User(BaseModel):
+    username: str
+    email: str
+
+class UserInDb(User):
+    password:str
+
+db = {
+    "javier":{
+        "username": "javier",
+        "email": "javier@admin.com",
+        "hashed_password":'$2b$12$ISuhZirZoIBqzlYbnyRlleuH28Qptbd4RbV9nJKbo3Uxj/kKNAWH2'
+    }
+}
+
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+token_oauth2 = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
-movies : List[Movie] = []
+
+async def verify_password(plain_password, hashed_password):
+    return pwd.verify(plain_password, hashed_password)
+
+async def get_password_hash(plain_password):
+    return pwd.hash(plain_password)
+
+
+
 @app.get('/', tags=['home'])
 async def home():
-    return 'my home'
+    try:
+        return 'YOU ARE HOME NOW'
+    except:
+        return 'ERROR OCURRED'
+    
 
-@app.post('/movie', tags=['movies'])
-async def create_movie(movie:Movie) -> List[Movie]:
-    movies.append(movie)
-    return movies
-
-@app.get('/movie/{id}', tags=['movies'])
-async def get_movie(id:int):
-    for movie in movies:
-        if movie.id == id:
-            return movie
-    return 'bad ID provided'
-@app.put('/movie/{id}', tags=['movies'])
-async def update_movie(id:int, movie:Movie) -> List[Movie]:
-    for item in movies:
-        if item.id == id:
-            item.id = movie.id
-            item.name = movie.name
-            item.star = movie.star
-            return movies
-    return 'not found'
